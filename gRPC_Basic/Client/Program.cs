fbbdf3f17caa7fda3241625033195201389cb9a7
venkatesh kuppusamy <kuppusamy_v@subway.com>
2021-06-13 23:54:26 +0530

    Chagnes to Client project

--- gRPC_Basic/Client/Program.cs
+++ gRPC_Basic/Client/Program.cs
@@ -6,7 +6,17 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Initializing gRPC client...");
+
+            Grpc.Core.Channel channel = new Grpc.Core.Channel("localhost", 5001, Grpc.Core.ChannelCredentials.Insecure);
+
+            var client = new CalculatorPkg.CalculatorService.CalculatorServiceClient(channel);
+
+            var result = client.Add(new CalculatorPkg.CalcRequest() { A = 2, B = 3 });
+
+            Console.WriteLine($"Sum of 2 and 3 is {result.Result}");
+
+            Console.ReadLine();
         }
     }
 }
--- gRPC_Basic/Server/APIs/CalculatorAPI.cs
+++ gRPC_Basic/Server/APIs/CalculatorAPI.cs
@@ -12,12 +12,14 @@ namespace Server.APIs
     {
         public override Task<CalcResponse> Add(CalcRequest request, ServerCallContext context)
         {
+            Console.WriteLine($"Request for Adding {request.A} and {request.B} received ");
             return Task.FromResult(new CalcResponse() { Result = request.A + request.B  });
 
         }
 
         public override Task<CalcResponse> Subtract(CalcRequest request, ServerCallContext context)
         {
+            Console.WriteLine($"Request for Subtracting {request.A} and {request.B} received ");
             return Task.FromResult(new CalcResponse() { Result = request.A - request.B });
         }
     }
